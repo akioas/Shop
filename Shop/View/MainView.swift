04bddb2d14afd51ac642a5089ff7d9e9f115f6d4
Ekaterina Velgusheva <evelgusheva@rts-soft.ru>
2022-12-15 20:11:53 +0300

    Launch screen, Color and Font assets

--- Shop/View/MainView.swift
+++ Shop/View/MainView.swift
@@ -10,43 +10,57 @@ struct MainView: View {
     
     
     var body: some View {
-        VStack {
-            if !isShowingSettings {
+        ZStack {
+            Color("Background") 
+                .ignoresSafeArea()
+            VStack {
+                if !isShowingSettings {
+                    Button(action: {
+                        Coordinator.push(view: MyCartView())
+                    })
+                    {
+                        Image(systemName: "globe")
+                            .imageScale(.large)
+                            .foregroundColor(.accentColor)
+                    }
+                }
                 Button(action: {
-                    Coordinator.push(view: MyCartView())
+                    Coordinator.push(view: ProductDetailsView())
                 })
                 {
-                    Image(systemName: "globe")
+                    Image(systemName: "flame")
                         .imageScale(.large)
                         .foregroundColor(.accentColor)
                 }
-            }
-            Button(action: {
-                Coordinator.push(view: ProductDetailsView())
-            })
-            {
-                Image(systemName: "flame")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-            }
-            .onAppear {
-                viewModel.getData()
-                isShowingSettings = false
-            }
-            Text(viewModel.viewData?.bestSeller.first?.title ?? "MAIN")
-            Button{
-                withAnimation{
-                    isShowingSettings.toggle()
+                .onAppear {
+                    viewModel.getData()
+                    isShowingSettings = false
                 }
-            } label: {
-                Text("SETTINGS")
+                Text(viewModel.viewData?.bestSeller.first?.title ?? "MAIN")
+                Button{
+                    withAnimation{
+                        isShowingSettings.toggle()
+                    }
+                } label: {
+                    Text("SETTINGS")
+                }
             }
+            .padding()
+            
+            SettingsView(isShowing: $isShowingSettings)
         }
-        .padding()
-    
-        
-        SettingsView(isShowing: $isShowingSettings)
         
     }
     
+    
+    
+}
+
+
+
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
 }
